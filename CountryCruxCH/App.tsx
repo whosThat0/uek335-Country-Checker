952510931d9804954dfac6431e8749df281f9906
naica922 <150661049+naica922@users.noreply.github.com>
2025-06-04 11:32:10 +0200

    Create basic car project

--- CountryCruxCH/App.tsx
+++ CountryCruxCH/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is our App</Text>
+      <Text>This is our car App</Text>
       <StatusBar style="auto" />
     </View>
   );
