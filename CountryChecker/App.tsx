058898c91cf72e0c5d671c03c7aa6154331f09d2
naica922 <150661049+naica922@users.noreply.github.com>
2025-06-05 15:47:39 +0200

    Change structure, used wrong template before

--- CountryChecker/App.tsx
+++ CountryChecker/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is our car App</Text>
+      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
